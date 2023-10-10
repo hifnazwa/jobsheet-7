29aa27683331a2462f36e6ddcb6acfa371e014b3
hifnazwa <144086644+hifnazwa@users.noreply.github.com>
2023-10-10 16:49:09 +0700

    Add files via upload

---
+++ DoWhileCuti14.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class DoWhileCuti14 {
+     public static void main(String[] args) {
+        Scanner sc14 = new Scanner (System.in);
+
+        int jatahCuti, jumlahHari;
+        String konfirmasi;
+
+        System.out.print("Jatah Cuti : ");
+        jatahCuti= sc14.nextInt();
+
+        do {
+            System.out.print("Apakah anda ingin mengambil cuti (y/t)? ");
+            konfirmasi = sc14.next();
+
+            if (konfirmasi.equalsIgnoreCase("y")) {
+                System.out.print("jumlah hari : ");
+                jumlahHari= sc14.nextInt();
+
+                if(jumlahHari <= jatahCuti) {
+                    jatahCuti-= jumlahHari;
+                    System.out.println("Sisa jatah cuti: " + jatahCuti);
+                } else {
+                    System.out.println("Sisa jatah anda tidak mencukupi");
+                    break;
+                }
+            }
+        } while (jatahCuti>0);
+    }
+
+}
