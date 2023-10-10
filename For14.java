6ee71d59270b0b3c4b1ac9a0a32f2428be4d36cc
hifnazwa <144086644+hifnazwa@users.noreply.github.com>
2023-10-10 22:42:06 +0700

    Add files via upload

---
+++ For14.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class For14 {
+    public static void main(String[] args) {
+        Scanner input09 = new Scanner(System.in);
+        
+        for (int i = 1; i <= 5; i++ ) {
+            System.out.println("Hasil kuadrat dari " + i + " adalah" + (i * i));
+        } 
+    }
+
+}
